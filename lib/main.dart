93c55c18d5ab1d59f8e4a015ecd0c2dd8c50317b
kisah <data.kisahtegar@gmail.com>
2022-12-23 21:39:30 +0700

    Dependency Injection: Creating dependency injection.

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,14 @@ import 'package:hmsi_app/features/presentation/pages/main_screen/main_screen.dar
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'on_generate_route.dart';
+import 'injection_container.dart' as di;
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  await di.init();
   runApp(const MyApp());
 }
 
